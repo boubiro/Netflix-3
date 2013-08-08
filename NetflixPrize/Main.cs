20be37297c927988fff5033d385fec44dba9a210
Benoit Gaillard <shareff.la.truffe@gmail.com>
2013-08-08 11:36:46 +0200

    NetflixPrize files added

---
+++ NetflixPrize/Main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace NetflixPrize
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			
+			throw new Exception("Aller les bleus, on est tous ensemble");
+		}
+	}
+}
